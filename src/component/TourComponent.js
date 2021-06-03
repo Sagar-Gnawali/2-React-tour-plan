12b82fbd429d1fc758fd7d5be660d41644f68245
Sagar Gnawali <sagargnawali2@gmail.com>
2021-06-03 13:37:16 +0545

    data is fetched and passed to the component

---
+++ src/component/TourComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const TourComponent = () => {
+    return (
+        <div>
+            <h1>TourCompoent </h1>
+        </div>
+    )
+}
+export default TourComponent;
---
+++ src/component/ToursComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Tour from './TourComponent.js';
+const ToursComponent = () => {
+    return (
+        <div>
+            <h1>Tours compoennts</h1>
+            <Tour/>
+        </div>
+    )
+}
+export default ToursComponent;
